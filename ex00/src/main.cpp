2fd8564351c875475ccc8ab71bdbb919f9e70d79
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2024-11-04 12:46:45 +0100

    style(main.cpp): update output formatting for Animal and WrongAnimal tests to enhance readability with color coding

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 
 int main()
 {
-	std::cout << "----- Animal ----- " << std::endl;
+	std::cout << "\033[1;34m--- Animal tests ---\033[0m" << std::endl;
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
@@ -19,7 +19,7 @@ int main()
 	delete j;
 	delete i;
 
-	std::cout << "\n----- WrongAnimal ----- " << std::endl;
+	std::cout << "\033[1;34m\n--- WrongAnimal tests ---\033[0m" << std::endl;
 	const WrongAnimal* wrongMeta = new WrongAnimal();
 	const WrongAnimal* wrongI = new WrongCat();
 	std::cout << wrongI->getType() << " " << std::endl;
